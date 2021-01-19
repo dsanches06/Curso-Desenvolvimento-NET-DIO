95b121c82f1421b6baaeddef4f181768e35d7156
Danilson Sanches <dsanches06@outlook.com>
2021-01-19 23:45:14 +0000

    Utilização de annotations para validar campos e formatar view

--- CursoMVC/Models/Produto.cs
+++ CursoMVC/Models/Produto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,10 +8,17 @@ namespace CursoMVC.Models
 {
     public class Produto
     {
+        
         public int Id { get; set; }
+
+        [Display(Name ="Descrição")]
+        [Required (ErrorMessage = "O campo descrição é obrigatório")]
         public string Descricao { get; set; }
+
+        [Range(1, 100, ErrorMessage = "O valor minímo é 1 e valor maximo é 100")]
         public int Quantidade { get; set; }
 
+        [Display(Name = "Categoria")]
         public int CategoriaId { get; set; }
         public Categoria Categoria { get; set; }
     }
